d2ce414680c2477fba326647dc493a6c2800f8fa
arulsuju <arulsuju@yahoo.co.in>
2021-03-12 00:04:00 +0530

    First Exercise

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+xs = np.linspace(0, 9, 100)
+ys = np.sin(xs)
+plt.plot(xs, ys, label="First application")
+plt.legend()
